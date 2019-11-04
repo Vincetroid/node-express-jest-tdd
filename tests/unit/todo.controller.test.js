cbca5a6e9553494b899c48ac3112747bad678e64
Vincetroid <vince_trance@hotmail.com>
2019-11-04 16:58:30 -0600

    Until beginning 16

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -1,14 +1,30 @@
 const TodoController = require("../../controllers/todo.controller");
 const TodoModel = require("../../model/todo.model");
+const httpMocks = require("node-mocks-http");
+const newTodo = require("../mock-data/new-todo.json");
 
 TodoModel.create = jest.fn();
+
+let req, res, next;
+beforeEach(() => {
+  req = httpMocks.createRequest();
+  res = httpMocks.createResponse();
+  next = null;
+});
 
 describe('TodoController.createTodo', () => {
   test("should have a createTodo function", () => {
     expect(typeof TodoController.createTodo).toBe("function");
   });
   test("should call TodoModel.create", () => {
-    TodoController.createTodo();
-    expect(TodoModel.create).toBeCalled();
+    req.body = newTodo;
+    TodoController.createTodo(req, res, next);
+    expect(TodoModel.create).toBeCalledWith(newTodo);
+  });
+  test('should return 201 response code', () => {
+    req.body = newTodo;
+    TodoController.createTodo(req, res, next);
+    expect(res.statusCode).toBe(201);
+    expect(res._isEndCalled()).toBeTruthy();
   });
 });
