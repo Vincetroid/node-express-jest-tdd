ae0b3aa86402046ebd402a7a6127f8afb0ea60f9
Vincetroid <vince_trance@hotmail.com>
2019-11-06 11:26:27 -0600

    Till beginning 38

--- tests/integration/todo.controller.int.test.js
+++ tests/integration/todo.controller.int.test.js
@@ -1,4 +1,3 @@
-
 const request = require("supertest");
 const app = require("../../app");
 const newTodo = require("../mock-data/new-todo.json");
@@ -13,5 +12,14 @@ describe(endpointUrl, () => {
     expect(response.statusCode).toBe(201);
     expect(response.body.title).toBe(newTodo.title);
     expect(response.body.done).toBe(newTodo.done);
+  });
+  test("should return error 500 on malformed data with POST" + endpointUrl, async () => {
+    const response = await request(app)
+      .post(endpointUrl)
+      .send({title: "Missing done property"})
+      expect(response.statusCode).toBe(500);
+      expect(response.body).toStrictEqual({
+        message: "Todo validation failed: done: Path `done` is required."
+      });
   });
 });
